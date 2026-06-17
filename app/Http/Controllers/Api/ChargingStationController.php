6258a3f222c9eb6e014f23c78c48b45e873189ec
rsokkheng <sokkhengry168@gmail.com>
2026-06-17 10:48:22 +0700

    fix

--- app/Http/Controllers/Api/ChargingStationController.php
+++ app/Http/Controllers/Api/ChargingStationController.php
@@ -16,7 +16,7 @@ class ChargingStationController extends ApiController
 
         if (is_numeric($lat) && is_numeric($lng)) {
             // Haversine distance in km, sort nearest first
-            $query->selectRaw("*, ( 6371 * acos( cos(radians(?)) * cos(radians(lat)) * cos(radians(lng) - radians(?)) + sin(radians(?)) * sin(radians(lat)) ) ) AS distance_km", [$lat, $lng, $lat])
+            $query->selectRaw("*, ( 6371 * acos( cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)) ) ) AS distance_km", [$lat, $lng, $lat])
                   ->orderBy('distance_km');
         } else {
             $query->orderBy('name');
