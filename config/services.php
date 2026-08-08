81ac81eeba8c5352b20a6ffb21b49b4ed98edef7
rsokkheng <sokkhengry168@gmail.com>
2026-08-08 14:12:37 +0700

    fix

--- config/services.php
+++ config/services.php
@@ -61,7 +61,7 @@ return [
     'plasgate' => [
         'private_key' => env('PLASGATE_PRIVATE_KEY'),
         'secret'      => env('PLASGATE_SECRET'),
-        'sender'      => env('PLASGATE_SENDER', 'AutoRide'),
+        'sender'      => env('PLASGATE_SENDER', 'ROTEH'),
         'url'         => env('PLASGATE_URL', 'https://cloudapi.plasgate.com/rest/send'),
     ],
 
